f81f7b6182670f7daeadccc2c34aa14e96205de0
DELL <bechir.karmeni@polytechnicien.com>
2023-08-02 02:27:31 +0100

    Initial commit

---
+++ lib/tcpdf_include.php
@@ -0,0 +1,24 @@
+<?php
+
+ * Search and include the TCPDF library.
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Include the main class.
+ * @author Nicola Asuni
+ * @since 2013-05-14
+ */
+
+// always load alternative config file for examples
+require_once('tcpdf_config_alt.php');
+
+// Include the main TCPDF library (search the library on the following directories).
+$tcpdf_include_dirs = array(realpath('tcpdf.php'), '/usr/share/php/tcpdf/tcpdf.php', '/usr/share/tcpdf/tcpdf.php', '/usr/share/php-tcpdf/tcpdf.php', '/var/www/tcpdf/tcpdf.php', '/var/www/html/tcpdf/tcpdf.php', '/usr/local/apache2/htdocs/tcpdf/tcpdf.php');
+foreach ($tcpdf_include_dirs as $tcpdf_include_path) {
+	if (@file_exists($tcpdf_include_path)) {
+		require_once($tcpdf_include_path);
+		break;
+	}
+}
+
+//============================================================+
+// END OF FILE
+//============================================================+
